d0d08ed057b36af475deb1b29c2f0af82ad4c2f6
dmzlingyin <dmzlingyin@163.com>
2023-09-08 00:03:48 +0800

    read: GET

--- routergroup.go
+++ routergroup.go
@@ -242,6 +242,7 @@ func (group *RouterGroup) combineHandlers(handlers HandlersChain) HandlersChain 
 	finalSize := len(group.Handlers) + len(handlers)
 	assert1(finalSize < int(abortIndex), "too many handlers")
 	mergedHandlers := make(HandlersChain, finalSize)
+	// 为什么不用 append
 	copy(mergedHandlers, group.Handlers)
 	copy(mergedHandlers[len(group.Handlers):], handlers)
 	return mergedHandlers
